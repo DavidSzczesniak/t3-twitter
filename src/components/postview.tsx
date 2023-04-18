afd09e1bf99ea1e532c5ceb2913d3a52638a48eb
DavidSzczesniak <david.szczesniak86@gmail.com>
2023-04-18 17:53:54 +0100

    fix post view profile url

--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -14,7 +14,7 @@ export const PostView = (props: PostWithUser) => {
 
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <Link href={`@${author.username}`}>
+      <Link href={`/@${author.username}`}>
         <Image
           src={author.profileImageUrl}
           alt={`@${author.username}'s profile picture`}
@@ -25,7 +25,7 @@ export const PostView = (props: PostWithUser) => {
       </Link>
       <div className="flex flex-col">
         <div className="flex text-slate-300">
-          <Link href={`@${author.username}`}>
+          <Link href={`/@${author.username}`}>
             <span>{`@${author.username}`}</span>
           </Link>
           <span className="px-2 text-slate-400">·</span>
