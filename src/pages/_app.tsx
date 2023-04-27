ea5dd129f1c3283d32adf7192192700db49aac26
DavidSzczesniak <david.szczesniak86@gmail.com>
2023-04-27 22:15:23 +0100

    add navbar with back button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,26 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { BsArrowLeftShort } from "react-icons/bs";
+
+const Navigation = () => {
+  const router = useRouter();
+  console.log("router", router);
+  return (
+    <nav className="flex justify-center">
+      <div className="flex w-full border-x border-slate-400 p-4 align-middle md:max-w-2xl">
+        {router.pathname === "/" ? (
+          <span className="text-lg font-bold">Home</span>
+        ) : (
+          <button type="button" onClick={() => router.back()}>
+            <BsArrowLeftShort size="2.25rem" />
+          </button>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
@@ -15,6 +35,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         <meta name="description" content="🤨" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navigation />
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
